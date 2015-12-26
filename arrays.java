d7544fd97aa86be8350b16ba319cc6a511a41b5d
Jeffrey Wan <Jwan622@gmail.com>
2015-12-26 13:22:28 -0500

    java arrays

--- arrays.java
+++ arrays.java
@@ -5,6 +5,21 @@ public class arrays {
 
     System.out.println(arr.length);
 
-    
+    arr[0] = 4;
+    arr[1] = arr[0] + 5;
+
+    System.out.println(arr[1]);
+
+    int[] arr1 = new int[5];
+    arr1[4] = 4;
+
+    System.out.println(arr1[4]);
+
+    int[] arr2 = {1,2,3,4,5};
+
+    System.out.println(arr2[0]);
+
+    // this line outputs something funny... pointer in memory perhaps?
+    System.out.println(arr2);
   }
 }
